53d29f63dcd5d7e873b6bb493a5d56cf798f87b1
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-07 16:42:32 -0700

    add routes to components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,10 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar';
 import About from './About';
+import Services from './Services';
+import Home from './Home';
+import Projects from './Projects';
+import Contact from './Contact';
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/about" Component={About} />
+        <Route path="/Services" Component={Services} />
+        <Route path="/" Component={Home} />
+        <Route path="/Projects" Component={Projects} />
+        <Route path="/Contact" Component={Contact} />
       </Routes>
     </Router>
   )
