f7a0e49535775fd15429befc593cb563cb5d390f
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-07 16:42:10 -0700

    add routes to components

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,16 @@
 import logo from './../assets/LucyLogo.png';
+import { Link } from 'react-router-dom';
 // import { slide as Menu } from 'react-burger-menu';
 // import { useMediaQuery } from 'react-responsive';
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <a>About</a>
-      <a>Services</a>
-      <img src={logo} className="logo"></img>
-      <a>Projects</a>
-      <a>Contact</a>
+      <Link to="/about">About</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/"><img src={logo} className="logo"></img></Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/contact">Contact</Link>
     </nav>
   )
 }
