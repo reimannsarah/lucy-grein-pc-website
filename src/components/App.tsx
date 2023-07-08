d252e66b5f18987919ebf634778c5d78938c01cc
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-07 21:03:24 -0700

    refactor to render landing page before other pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,43 @@
-import './App.css';
+import '../styles/App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar';
-import About from './About';
+import About from './About/About';
 import Services from './Services';
 import Home from './Home';
 import Projects from './Projects';
 import Contact from './Contact';
+import React, { ReactNode } from 'react';
+import LandingPage from './LandingPage/LandingPage';
+
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  return(
+    <React.Fragment>
+      <NavBar />
+      {children}
+    </React.Fragment>
+  )
+}
 
 function App() {
   return (
     <Router>
-      <NavBar />
       <Routes>
-        <Route path="/about" Component={About} />
-        <Route path="/Services" Component={Services} />
-        <Route path="/" Component={Home} />
-        <Route path="/Projects" Component={Projects} />
-        <Route path="/Contact" Component={Contact} />
+        <Route path='/' Component={LandingPage}></Route>
+        <Route 
+          element={
+            <MainLayout>
+              <Route path="/about" Component={About} />
+              <Route path="/Services" Component={Services} />
+              <Route path="/" Component={Home} />
+              <Route path="/Projects" Component={Projects} />
+              <Route path="/Contact" Component={Contact} />
+            </MainLayout>
+          }
+        />
       </Routes>
     </Router>
   )
