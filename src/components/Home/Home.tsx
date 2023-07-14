f1df4cc770334f6e0aeec7cad0e67b52d92b3337
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-14 13:03:46 -0700

    style blue boxes with grid

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,30 +5,37 @@ const Home: React.FC = () => {
   return (
     <div>
       <div>
-        <img src={LucyBubba} />
+        <img className="pic" src={LucyBubba} />
         <h3>Hi, I'm Lucy</h3>
         <p>I am a parent Consultant and I'm here to make parenting feel accesible, functional, and intuitive</p>
       </div>
       <div className="boxes">
+        <div className="parent-box">
           <div className="box1">
-            <p>lorem ipsum</p>
+            <p className="title">Bio</p>
           </div>
           <div className="box2">
-            <p>lorem ipsum</p>
+            <p className="quote">Working with families is my passion</p>
           </div>
-        
-        {/* <div className="box1">
-          <p>lorem ipsum</p>
         </div>
-        <div className="box2">
-          <p>lorem ipsum</p>
+
+        <div className="parent-box">
+          <div className="box1">
+            <p className="title">Programs</p>
+          </div>
+          <div className="box2">
+            <p className="quote">I believe parents deserve support and collaboration</p>
+          </div>
         </div>
-        <div className="box1">
-          <p>lorem ipsum</p>
+
+        <div className="parent-box">
+          <div className="box1">
+            <p className="title">Intuition Theory</p>
+          </div>
+          <div className="box2">
+            <p className="quote">Intuition is a complex system of values, perception, and memory</p>
+          </div>
         </div>
-        <div className="box2">
-          <p>lorem ipsum</p>
-        </div> */}
       </div>
 
     </div>
