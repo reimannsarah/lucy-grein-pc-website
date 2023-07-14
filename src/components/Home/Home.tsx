bc0349c4c2f84439b13fe3fb6dea06901cf84784
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-14 10:36:57 -0700

    WIP working on styling navigation boxes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,13 +5,49 @@ const Home: React.FC = () => {
   return (
     <div>
       <div>
-        <img src={LucyBubba} />
+        <img className="pic" src={LucyBubba} />
         <h3>Hi, I'm Lucy</h3>
         <p>I am a parent Consultant and I'm here to make parenting feel accesible, functional, and intuitive</p>
       </div>
-      <svg  className="box" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
-        <rect width="100" height="100" />
-      </svg>
+      <div className="box-container">
+
+        <div className="boxes">
+
+          <svg className="box1" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 100 100">
+            <rect width="200" height="200" />
+          </svg>
+
+          <svg className="box2" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 100 100">
+            <rect width="200" height="200" />
+          </svg>
+
+        </div>
+
+        <div className="boxes">
+
+          <svg className="box1" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 100 100">
+            <rect width="200" height="200" />
+          </svg>
+
+          <svg className="box2" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 100 100">
+            <rect width="200" height="200" />
+          </svg>
+
+        </div>
+        
+        <div className="boxes">
+
+          <svg className="box1" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 100 100">
+            <rect width="200" height="200" />
+          </svg>
+
+          <svg className="box2" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 100 100">
+            <rect width="200" height="200" />
+          </svg>
+
+        </div>
+
+      </div>
 
     </div>
   )
