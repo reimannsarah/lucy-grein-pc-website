832ff1df4219fee648a1405472c2a581f456da8a
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-21 13:26:12 -0700

    create navbar folder and move necessary files into it

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
-import logo from './../assets/imgs/LucyLogo.png';
+import logo from '../../assets/imgs/LucyLogo.png';
 import { Link } from 'react-router-dom';
-import '../styles/NavBar.css'
+import './NavBar.css'
 // import { slide as Menu } from 'react-burger-menu';
 // import { useMediaQuery } from 'react-responsive';
 
