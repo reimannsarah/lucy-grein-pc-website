8d5ed1f5ea8ccc1fb9c08d4847425ee6798e16d2
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-22 11:34:49 -0700

    WIP working on dropdown menus

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,11 +7,32 @@ import './NavBar.css'
 function NavBar() {
   return (
     <nav className="navbar">
-      <Link to="/about" className="navbar-link" id="about-nav">About</Link>
-      <Link to="/services" className="navbar-link" id="services-nav">Services</Link>
+      <div className="dropdown">
+        <Link to="/about" className="navbar-link" id="about-nav">About</Link>
+        <div className="dropdown-content">
+          <Link to="/bio">Bio</Link>
+          <Link to="/values">Values</Link>
+        </div>
+      </div>
+      <div className="dropdown">
+        <Link to="/services" className="navbar-link" id="services-nav">Services</Link>
+        <div className="dropdown-content">
+          <Link to="/programs">Programs</Link>
+          <Link to="/testimony">Testimoy</Link>
+          <Link to="/FAQ">FAQ</Link>
+        </div>
+      </div>
       <Link to="/home" className="navbar-links"><img src={logo} className="logo"></img></Link>
-      <Link to="/projects" className="navbar-link" id="projects-nav">Projects</Link>
-      <Link to="/contact" className="navbar-link" id="contact-nav">Contact</Link>
+      <div className="dropdown">
+        <Link to="/projects" className="navbar-link" id="projects-nav">Projects</Link>
+        <div className="dropdown-content">
+          <Link to="/essays">Essays</Link>
+          <Link to="/intuition-theory">Intuition Theory</Link>
+        </div>
+      </div>
+      <div className="dropdown">
+        <Link to="/contact" className="navbar-link" id="contact-nav">Contact</Link>
+      </div>
     </nav>
   )
 }
