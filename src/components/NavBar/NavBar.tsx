0813b3603f252e076a532a785bb672d8b834947c
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-23 16:32:22 -0700

    WIP still working on drop down menus

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,32 +7,41 @@ import './NavBar.css'
 function NavBar() {
   return (
     <nav className="navbar">
+
       <div className="dropdown">
         <Link to="/about" className="navbar-link" id="about-nav">About</Link>
-        <div className="dropdown-content">
+        <div className="dropdown-content" id="about-menu">
           <Link to="/bio">Bio</Link>
           <Link to="/values">Values</Link>
         </div>
       </div>
+
       <div className="dropdown">
         <Link to="/services" className="navbar-link" id="services-nav">Services</Link>
-        <div className="dropdown-content">
+        <div className="dropdown-content" id="services-menu">
           <Link to="/programs">Programs</Link>
           <Link to="/testimony">Testimoy</Link>
           <Link to="/FAQ">FAQ</Link>
         </div>
       </div>
+
       <Link to="/home" className="navbar-links"><img src={logo} className="logo"></img></Link>
+
       <div className="dropdown">
         <Link to="/projects" className="navbar-link" id="projects-nav">Projects</Link>
-        <div className="dropdown-content">
+        <div className="dropdown-content" id="projects-menu">
           <Link to="/essays">Essays</Link>
           <Link to="/intuition-theory">Intuition Theory</Link>
         </div>
       </div>
+
       <div className="dropdown">
         <Link to="/contact" className="navbar-link" id="contact-nav">Contact</Link>
+        <div className="dropdown-content" id="contact-menu">
+          <Link to="/something">Something</Link>
+        </div>
       </div>
+
     </nav>
   )
 }
