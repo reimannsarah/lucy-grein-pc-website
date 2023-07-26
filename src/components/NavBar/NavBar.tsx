4981e744065087b50972a4e023f7da70de1b733a
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-26 13:00:50 -0700

    add nodeRef to CSSTransitions to solve error

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './NavBar.css'
 import { useState } from 'react';
 import { CSSTransition } from 'react-transition-group';
+import React from 'react';
 
 
 function NavBar() {
@@ -10,6 +11,7 @@ function NavBar() {
   const [activeServicesDropDown, setActiveServicesDropDown] = useState(false);
   const [activeProjectsDropDown, setActiveProjectsDropDown] = useState(false);
   const [activeContactDropDown, setActiveContactDropDown] = useState(false);
+  const nodeRef = React.useRef(null);
 
   function handleAboutEnter() {
     setActiveAboutDropDown(true);
@@ -42,14 +44,14 @@ function NavBar() {
   function handleContactLeave() {
     setActiveContactDropDown(false);
   }
-
+  
   return (
     <nav className="navbar">
 
       <div className='dropdown' onMouseEnter={handleAboutEnter}
         onMouseLeave={handleAboutLeave}>
         <Link to="/about" className="navbar-link" id="about-nav">About</Link>
-        <CSSTransition in={activeAboutDropDown} timeout={300} classNames="transition" unmountOnExit>
+        <CSSTransition nodeRef={nodeRef} in={activeAboutDropDown} timeout={300} classNames="transition" unmountOnExit>
 
           <div className="dropdown-content" id="about-menu">
             <Link to="/bio">Bio</Link>
@@ -62,7 +64,7 @@ function NavBar() {
         onMouseLeave={handleServicesLeave}>
         <Link to="/services" className="navbar-link" id="services-nav">Services</Link>
 
-        <CSSTransition in={activeServicesDropDown} timeout={300} classNames="transition" unmountOnExit>
+        <CSSTransition nodeRef={nodeRef} in={activeServicesDropDown} timeout={300} classNames="transition" unmountOnExit>
           <div className="dropdown-content" id="services-menu">
             <Link to="/programs">Programs</Link>
             <Link to="/testimony">Testimoy</Link>
@@ -76,7 +78,8 @@ function NavBar() {
       <div className="dropdown" onMouseEnter={handleProjectsEnter}
         onMouseLeave={handleProjectsLeave}>
         <Link to="/projects" className="navbar-link" id="projects-nav">Projects</Link>
-        <CSSTransition in={activeProjectsDropDown} timeout={300} classNames="transition" unmountOnExit>
+
+        <CSSTransition nodeRef={nodeRef} in={activeProjectsDropDown} timeout={300} classNames="transition" unmountOnExit>
           <div className="dropdown-content" id="projects-menu">
             <Link to="/essays">Essays</Link>
             <Link to="/intuition-theory">Intuition Theory</Link>
@@ -87,7 +90,8 @@ function NavBar() {
       <div className="dropdown" onMouseEnter={handleContactEnter}
         onMouseLeave={handleContactLeave}>
         <Link to="/contact" className="navbar-link" id="contact-nav">Contact</Link>
-        <CSSTransition in={activeContactDropDown} timeout={300} classNames="transition" unmountOnExit>
+
+        <CSSTransition nodeRef={nodeRef} in={activeContactDropDown} timeout={300} classNames="transition" unmountOnExit>
           <div className="dropdown-content" id="contact-menu">
             <Link to="/something">Something</Link>
           </div>
