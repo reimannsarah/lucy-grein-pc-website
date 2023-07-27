b8dff4fa2af057fe7de625f68bfe18bd8ac247c9
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-26 18:40:58 -0700

    remove about and add values

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
 import '../styles/App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
-import About from './About/About';
 import Services from './Services';
+import Values from './Values/Values';
 import Home from './Home/Home';
 import Projects from './Projects';
 import Contact from './Contact';
@@ -11,7 +11,8 @@ import LandingPage from './LandingPage/LandingPage';
 import Footer from './Footer/Footer';
 import Bio from './Bio/Bio';
 import ScrollToTop from './ScrollToTop.tsx';
-import { TransitionGroup } from 'react-transition-group'; 
+import { TransitionGroup, CSSTransition } from 'react-transition-group'; 
+import '../styles/App.css';
 // import { useState } from 'react';
 
 interface MainLayoutProps {
@@ -34,15 +35,17 @@ function App() {
     <Router>
       <ScrollToTop/>
       <TransitionGroup>
+        <CSSTransition timeout={450} classNames="fade">
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<MainLayout><About /></MainLayout>} />
         <Route path="/bio" element={<MainLayout><Bio /></MainLayout>} />
+        <Route path="/values" element={<MainLayout><Values /></MainLayout>} />
         <Route path="/services" element={<MainLayout><Services /></MainLayout>} />
         <Route path="/projects" element={<MainLayout><Projects /></MainLayout>} />
         <Route path="/contact" element={<MainLayout><Contact /></MainLayout>} />
         <Route path="/home" element={<MainLayout><Home /></MainLayout>} />
       </Routes>
+      </CSSTransition>
       </TransitionGroup>
     </Router>
   )
