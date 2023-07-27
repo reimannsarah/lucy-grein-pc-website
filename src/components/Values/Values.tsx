e05164a964585a9437cb78cb28bb77a1cc5012d4
Sarah Reimann <reimannsarah45@gmail.com>
2023-07-26 19:25:49 -0700

    add values content and initial styling

--- src/components/Values/Values.tsx
+++ src/components/Values/Values.tsx
@@ -1,36 +1,49 @@
-import LucyBubba from '../../assets/imgs/LucyBubba.png';
+import beach from '../../assets/imgs/beach.png';
 import './Values.css'
 
 function Values() {
   return (
     <div>
       <div className="values-top-div">
-        <h1>Values and Philosophies</h1>
-        <p>My core values have been shaped by life experience. I prioritize
-          these things in all ways by which I move through the world.
-          My philosophies of engagement are informed by my various
-          forms of training and education. They are how I work with
-          children and connect with families effectively.
-          My website has written descriptions of each value and
-          philosophy. </p>
-        <img src={LucyBubba} alt="lu and bub" />
+
+        <div className="values-title-text">
+          <h1 id="values-title">Values & Philosophies</h1>
+          <p>My core values have been shaped by life experience. I prioritize
+            these things in all ways by which I move through the world.
+            My philosophies of engagement are informed by my various
+            forms of training and education. They are how I work with
+            children and connect with families effectively.
+            My website has written descriptions of each value and
+            philosophy. </p>
+        </div>
+
+        <img id="beach" src={beach} alt="lu and bub" />
       </div>
+        <div className="core-values-title"><h1>Core Values</h1></div>
       <div className="core-values">
-        <p>Community</p>
-        <p>Resilience</p>
-        <p>Communication</p>
-        <p>Connection</p>
-        <p>Comfort</p>
-        <p>Self-Reflection</p>
-        <p>Creatvity</p>
-        <p>Responsibility</p>
-        <p>Accessibility</p>
+        <div>
+          <p>Community</p>
+          <p>Resilience</p>
+          <p>Communication</p>
+        </div>
+        <div>
+          <p>Connection</p>
+          <p>Comfort</p>
+          <p>Self-Reflection</p>
+        </div>
+        <div>
+          <p>Creativity</p>
+          <p>Responsibility</p>
+          <p>Accessibility</p>
+        </div>
       </div>
       <div className="philosophies">
         <p>Boundaries create freedom</p>
         <p>Trial and error</p>
         <p>Learning happens in discomfort</p>
-        <p>Happt</p>
+        <p>Happy, healthy, and safe</p>
+        <p>All experiences are internalized</p>
+        <p>Centering the self allows for effective assessment</p>
       </div>
     </div>
   )
