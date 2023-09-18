092e47b4cc463e14ab36d6c9da5a195e7cd04ce0
Sarah Reimann <reimannsarah45@gmail.com>
2023-09-18 11:16:30 -0700

    add coming soon header to landing page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -15,12 +15,15 @@ const LandingPage: React.FC = () => {
         <h1 className="lucy-grein">Lucy Grein</h1>
         <p className="parent-consultant">Parent Consultant</p>
       </div>
-      <div>
+      <div className="coming-soon">
+        <h1>Website Coming Soon!</h1>
+      </div>
+      {/* <div>
         <nav  className="buttons">
             <button onClick={() => handleExploreClick('/journey')}>Take The Journey</button>
             <button onClick={() => handleExploreClick('/home')} >Explore On My Own</button>
         </nav>
-      </div>
+      </div> */}
     </div>
   )
 }
